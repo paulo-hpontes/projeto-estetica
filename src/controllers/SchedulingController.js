7d16bebb5389bebbd4bcb749ae9e9f5e9fe3de62
paulo-hpontes <pauloh.teles16@gmail.com>
2024-10-19 15:17:50 -0400

    Agendamento
    Inicio da função de agendamento

--- src/controllers/SchedulingController.js
+++ src/controllers/SchedulingController.js
@@ -1,5 +1,5 @@
 const Scheduling = require("../models/Scheduling");
-const User = require('../models/User');
+const User = require("../models/User");
 
 const newScheduling = async (req, res) => {
   const { date, time } = req.body;
@@ -10,21 +10,22 @@ const newScheduling = async (req, res) => {
     const userName = user.name;
     const userEmail = user.email;
 
-    if(!user){
-        return res.status(422).json({
-            errors: ['Houve um erro inesperado, por favor tente mais tarde']
-        })
+    if (!user) {
+      return res.status(422).json({
+        errors: ["Houve um erro inesperado, por favor tente mais tarde"],
+      });
     }
-    
+
     const newScheduling = await Scheduling.create({
-        date,
-        time,
-        userName,
-        userEmail
+      date,
+      time,
+      userName,
+      userEmail,
     });
 
-    return res.status(201).json({newScheduling, message: "Horário agendado com sucesso!"})
-
+    return res
+      .status(201)
+      .json({ newScheduling, message: "Horário agendado com sucesso!" });
   } catch (e) {
     console.log(e);
     return res.status(400).json({
@@ -34,29 +35,33 @@ const newScheduling = async (req, res) => {
 };
 
 const deleteScheduling = async (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
 
-  try{
+  try {
     let scheduling = await Scheduling.findById(id);
 
-    
     if (!scheduling) {
       res.status(422).json({ errors: ["Horário não existe!"] });
       return;
     }
-  
-    await Scheduling.deleteOne(scheduling);
-    return res.status(200).json({ message: 'Agendamento excluído com sucesso!'});
 
-  }catch(e){
+    await Scheduling.deleteOne(scheduling);
+    return res
+      .status(200)
+      .json({ message: "Agendamento excluído com sucesso!" });
+  } catch (e) {
     console.log(e);
     return res.status(400).json({
       errors: ["Houve um erro inesperado, por favor tente mais tarde"],
     });
   }
+};
+
+const getAllScheduling = async (req, res) => {
+  
 }
 
 module.exports = {
-    newScheduling,
-    deleteScheduling
-}
+  newScheduling,
+  deleteScheduling,
+};
