9052b92b2833fb1c06d916eef2c4cca67007adcf
paulo-hpontes <pauloh.teles16@gmail.com>
2024-11-16 20:01:38 -0400

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  mode: 'production', 
-  entry: './app.js',
+  mode: "production",
+  entry: "./app.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',  // Nome do arquivo final gerado
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js", // Nome do arquivo final gerado
   },
-  externals: [nodeExternals()],  // Exclui dependências do Node.js do bundle
+  externals: [nodeExternals()], // Exclui dependências do Node.js do bundle
   module: {
     rules: [
       {
-        test: /\.js$/,  // Aplica Babel a todos os arquivos .js
-        exclude: /node_modules/,  // Não transpile arquivos dentro de node_modules
+        test: /\.js$/, // Aplica Babel a todos os arquivos .js
+        exclude: /node_modules/, // Não transpile arquivos dentro de node_modules
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.json'],
+    extensions: [".js", ".json"],
+    fallback: {
+      path: require.resolve("path-browserify"),
+    },
   },
-  devtool: 'source-map',
+  devtool: "source-map",
 };
